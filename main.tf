82b078eb9b6362678c31f665e00ea207f11151b9
bruc31984 <52766521+bruc31984@users.noreply.github.com>
2023-03-14 16:38:12 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-# resource "aws_s3_bucket" "test_bucket" {
-#   bucket = "lilybucket123456"
-#   acl    = "public-read"
-#   policy = file("./policy.json")
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "lilybucket123456"
+  acl    = "public-read"
+  policy = file("./policy.json")
 
-#   website {
-#     index_document = "index.html"
-#     error_document = "error.html"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
 
-#     routing_rules = <<EOF
-# [{
-#     "Condition": {
-#         "KeyPrefixEquals": "docs/"
-#     },
-#     "Redirect": {
-#         "ReplaceKeyPrefixWith": "documents/"
-#     }
-# }]
-# EOF
-#   }
-# }
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
